f9380e3b3b1453982f18917ccdc3178a235b676d
nharini27 <41140729+nharini27@users.noreply.github.com>
2022-03-15 16:46:05 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Addition
+{
+	public static void main(String[] args) {
+		int a;
+		int b;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		int res =a+b;
+		System.out.println(res);
+	}
+}
